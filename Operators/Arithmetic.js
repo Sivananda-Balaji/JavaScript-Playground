91fcd7536899b8dff78f88ad0ce4578c13a97c73
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-01-11 15:53:41 +0530

    Add Operators folder

---
+++ Operators/Arithmetic.js
@@ -0,0 +1,28 @@
+// Addition
+
+let num1 = 12;
+let num2 = 3;
+
+console.log(num1+num2);
+
+//subtraction
+console.log(num1-num2);
+
+//Multiplication
+console.log(num1*num2);
+
+//division
+console.log(num1 / num2);
+
+//remainder
+console.log(num1%num2);
+
+//power
+console.log(num1 ** num2);
+
+/*
+In the above example if the num2 is expressed like num2 = "3". All the amswer will be same except the addition operator, it will concat the num1 and num2 (123).
+
+if the num1 or num2 conatains the value which can't be converted into numbers, all the opertions results as NaN (Not a Number) except the addition operators. It will concat the two values.
+
+*/
---
+++ Operators/Conversion.js
@@ -0,0 +1,20 @@
+// String to Number
+let value = "34";
+
+console.log(typeof Number(value), Number(value));
+
+console.log("------------");
+
+//Number to String
+let value_1 = 23;
+
+console.log(typeof String(value_1),String(value_1));
+
+console.log("------------");
+
+//Number,String to Boolean
+let val = 0;
+console.log(typeof Boolean(value),Boolean(value));
+console.log(typeof Boolean(value_1),Boolean(value_1));
+console.log(typeof Boolean(val),Boolean(val));
+
