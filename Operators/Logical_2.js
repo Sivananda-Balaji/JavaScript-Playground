ee40a4409f11a94ca12f558499ff3c80098dc76c
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-01-12 12:59:40 +0530

    Add logical 2 file

---
+++ Operators/Logical_2.js
@@ -0,0 +1,35 @@
+// More use of logical operator
+
+
+let firstName = "James";
+let lastName = "Bond";
+let fatherName = "Miller";
+
+// In the below statement, father's name will print if the last name is empty.
+console.log(`The Name is ${firstName} ${lastName || fatherName}`);
+console.log(`The Name is ${firstName} ${lastName && fatherName}`);
+
+/**
+ truthy - "a",1
+ falsy - "" , 0 , null , undefined
+
+ Short circuiting
+ OR - Search for first truth value and return, if the truthy value is not found return the last falsy value.
+ AND - Search for first falsy value and return, if the falsy value is not found return the last truthy value.
+
+ */
+
+
+console.log("--------------------")
+
+/**
+ * Nullish coalescing
+ * Nullish coalescing (??) - will look for another value if the first value is either undefined or null
+ */
+
+let num  = 0;
+let num2;
+
+console.log(num ?? 1);
+console.log(num2 ?? 1);
+
