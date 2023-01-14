ea137a5abd366ee68908c0c07482e75ab81cd944
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-01-14 17:31:32 +0530

    Add Error Handling Folder

---
+++ ErrorHandling/Error-Handling.js
@@ -0,0 +1,12 @@
+
+
+let num1 = 50 , num2 = 2;
+
+try {
+    console.log(num1/num2);
+    console.log(a);
+} catch(err){
+ console.log(err.message);
+} finally {
+    console.log("Finally Executed.");
+}
